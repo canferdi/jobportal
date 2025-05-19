be2fa32053f745dff0d41ea7bb44c179d03029b0
Ferdi Can <fcan5414@gmail.com>
2025-05-20 01:49:46 +0300

    first commit

---
+++ src/main/java/com/ferdican/jobportal/repository/UsersTypeRepository.java
@@ -0,0 +1,7 @@
+package com.ferdican.jobportal.repository;
+
+import com.ferdican.jobportal.entity.UsersType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+}
